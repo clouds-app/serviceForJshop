5e44902a8a8eb7815d02734e6cd64ed137dc0e12
hdeqi <hdeqi@qq.com>
2020-01-04 11:35:39 +0800

    add swagger ui and init project

--- app/router.js
+++ app/router.js
@@ -3,7 +3,127 @@
 /**
  * @param {Egg.Application} app - egg application
  */
-module.exports = app => {
-  const { router, controller } = app;
-  router.get('/', controller.home.index);
-};
+// module.exports = app => {
+//   const { router, controller } = app;
+//   router.get('/', controller.home.index);
+// };
+
+ // {app_root}/app/router.js
+  module.exports = app => {
+    const { router, controller, swagger } = app;
+    router.post('/', controller.home.index);
+    swagger.post('/', {
+        tags: [
+          'admin',
+        ],
+        summary: 'Login a admin',
+        description: '',
+        parameters: [
+          {
+            in: 'body',
+            name: 'body',
+            description: 'admin\'s username & password',
+            required: true,
+            schema: {
+              type: 'object',
+              required: [ 'username', 'password' ],
+              properties: {
+                username: {
+                  type: 'string',
+                  description: 'admin\'s username',
+                },
+                password: {
+                  type: 'string',
+                  description: 'admin\'s password',
+                },
+              },
+            },
+          },
+        ],
+        responses: {
+          200: {
+            description: 'SUCCEED',
+            schema: {
+              type: 'object',
+              properties: {
+                status: {
+                  type: 'string',
+                  description: 'status',
+                },
+                data: {
+                  type: 'object',
+                  description: 'data',
+                  properties: {
+                    token: {
+                      type: 'string',
+                      description: 'token',
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      });
+    router.get('/', controller.home.index);
+    swagger.get('/', {
+      tags: [
+        'role',
+      ],
+      summary: 'search role by page',
+      description: '',
+      parameters: [
+        {
+          in: 'query',
+          name: 'name',
+          description: 'role\'s name',
+        },
+        {
+          in: 'query',
+          name: 'pageIndex',
+          description: 'pageIndex',
+        },
+        {
+          in: 'query',
+          name: 'pageSize',
+          description: 'pageSize',
+        },
+      ],
+      responses: {
+        200: {
+           description: 'SUCCEED',
+           schema: {
+             type: 'object',
+             properties: {
+               status: {
+                 type: 'string',
+                 description: 'status',
+               },
+               datas: {
+                 type: 'array',
+                 description: 'result datas',
+                 properties: {
+                   token: {
+                     type: 'string',
+                     description: 'token',
+                   },
+                 },
+               },
+               pageIndex:{
+                 type: 'number',
+                 description: 'pageIndex',
+               },
+               pageSize:{
+                 type: 'number',
+                 description: 'pageSize',
+               },
+               totalCount:{
+                 type: 'number',
+                 description: 'totalCount',
+               },
+             },
+           },
+         },
+      },
+    });
+  };
--- config/config.default.js
+++ config/config.default.js
@@ -23,7 +23,56 @@ module.exports = appInfo => {
     // myAppName: 'egg',
   };
 
+ 
+ const swagger2 = {
+    enable:false, // disable swagger , default true
+    base: {
+      /* default config,support cover
+      schemes: [
+          'http',
+      ],
+      host: '127.0.0.1:7001',
+      basePath: '/',
+      consumes: [
+      'application/json',
+      ],
+      produces: [
+      'application/json',
+      ],
+      */
+      info: {
+        description: 'This is a test swagger-ui html',
+        version: '1.0.0',
+        title: 'TEST',
+        contact: {
+          email: 'caandoll@aliyun.com',
+        },
+        license: {
+          name: 'Apache 2.0',
+          url: 'http://www.apache.org/licenses/LICENSE-2.0.html',
+        },
+      },
+      tags: [
+        {
+          name: 'admin',
+          description: 'Admin desc',
+        },
+        {
+          name: 'role',
+          description: 'Role desc',
+        },
+      ],
+      definitions:{
+      // model definitions
+      },
+      securityDefinitions:{
+      // security definitions
+      }
+    },
+  }
+
   return {
+	...swagger2,
     ...config,
     ...userConfig,
   };
